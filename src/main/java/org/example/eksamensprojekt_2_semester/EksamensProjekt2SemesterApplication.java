8d60637fdf1584fa6fd005532379495342ed6a29
nicky4gun <nicklas.stagesorensen@outlook.dk>
2026-04-30 09:10:24 +0200

    created

---
+++ src/main/java/org/example/eksamensprojekt_2_semester/EksamensProjekt2SemesterApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojekt_2_semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensProjekt2SemesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksamensProjekt2SemesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/eksamensprojekt_2_semester/EksamensProjekt2SemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamensprojekt_2_semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamensProjekt2SemesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
